5bcdda0938f983d89408fd06ed3767c3466b4420
edsondesennajunior <edsonsjr62@gmail.com>
2020-09-19 16:17:03 -0300

    Creating cache cleanup function

--- sw.js
+++ sw.js
@@ -1,9 +1,47 @@
+const cacheName = 'app-shell-v1';
+const assetsToCache = [
+    'offline.html'
+];
+
+function removeOldCache(key) {
+    if(key !== cacheName) {
+        console.log(`[ServiceWorker] Removing old cache: ${key}`);
+        return caches.delete(key);
+    }
+}
+
+async function cacheCleanup() {
+    const keyList = await caches.key();
+    return Promise.all(keyList.map(removeOldCache));
+}
+
+async function cacheStaticAssets() {
+    const cache = await caches.open(cacheName);
+    return cache.addAll(assetsToCache);
+}
+
 self.addEventListener('install', event => {
     console.log('[ServiceWorker] Installing service worker...', event);
+    event.waitUntil(cacheStaticAssets());
     self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
     console.log('[ServiceWorker] Activating service worker...', event);
-    return self.ClientRectList.claim();
+    event.waitUntil(cacheCleanup());
+    return self.clients.claim();
 });
+
+async function networkFirst(request) {
+    try{
+        return await fetch(request);
+    } catch(err) {
+        const cache = await caches.open(cacheName);
+        return cache.match('offline.html');
+    }
+}
+
+self.addEventListener('fetch', event => {
+    console.log('[ServiceWorker] Fetch event...', event);
+    event.respondWith(networkFirst(event.request));
+})
